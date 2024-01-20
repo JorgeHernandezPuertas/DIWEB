230a9e022e646549d1b4d98f124376d1cac92c4d
JorgeHernandezPuertas <jherpue553@g.educaand.es>
2024-01-20 13:23:02 +0100

    Arreglo el boton de play al terminar la cancion y parseo los valores a int antes de hacer acciones

--- tema3/ejercicio1-audio/scripts/reproductorClicks.js
+++ tema3/ejercicio1-audio/scripts/reproductorClicks.js
@@ -32,7 +32,7 @@ tiempoActual.innerHTML = '00:00'
 audio.addEventListener('timeupdate', () => {
   const tiempo = Number.parseInt(audio.currentTime)
   tiempoActual.innerHTML = formatTime(tiempo)
-  barraDuracion.setAttribute('value', 100 / audio.duration * tiempo)
+  barraDuracion.setAttribute('value', Number.parseInt(100 / audio.duration * tiempo))
 })
 
 // Función que formatea el tiempo del audio al formato que quiero mostrar
@@ -47,13 +47,14 @@ function formatTime (time) {
 
 // Pongo el tiempo máximo de la canción y controlo sincronizar el cambio de duración
 tiempoMaximo.innerHTML = formatTime(audio.duration)
-audio.addEventListener('durationchange', () => {
-  tiempoMaximo.innerHTML = formatTime(audio.duration)
+audio.addEventListener('durationchange', (e) => {
+  tiempoMaximo.innerHTML = formatTime(e.target.duration)
 })
 
 // Controlo el cambio manual de la barra de duración
 barraDuracion.addEventListener('change', () => {
-  audio.currentTime = (barraDuracion.value / 100)
+  // Calculo el tiempo correspondiente al valor de la barra
+  audio.currentTime = (Number.parseInt(barraDuracion.value * audio.duration / 100))
 })
 
 // Doy funcionalidad a la barra de volumen
@@ -72,7 +73,7 @@ barraVolumen.addEventListener('change', () => {
 // Doy funcionalidad al boton del volumen (mute/unmuted)
 var volumenAnterior
 botonVolumen.addEventListener('click', () => {
-  if (botonVolumen.className === 'muted'){
+  if (botonVolumen.className === 'muted') {
     botonVolumen.className = 'unmuted'
     audio.muted = false
     audio.volume = volumenAnterior
@@ -87,4 +88,9 @@ botonVolumen.addEventListener('click', () => {
 // Controlo que cada vez que se cambie el volumen por cualquier lado, se sincronice la barra
 audio.addEventListener('volumechange', () => {
   barraVolumen.setAttribute("value", audio.volume * 100)
+})
+
+// Cuando acaba la canción reseteo el botón de play
+audio.addEventListener("ended", () => {
+  botonPlayPause.className = 'play';
 })
