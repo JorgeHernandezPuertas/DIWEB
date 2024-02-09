6a63d9b7bd40f6b31d29c9e14df984853cf5e335
jorge <jherpue553@g.educaand.es>
2024-02-09 11:13:06 +0100

    Añado la libreria y termino el toggle

--- jquery/tareasEventosRaton1/scripts/eventos.js
+++ jquery/tareasEventosRaton1/scripts/eventos.js
@@ -1,8 +1,18 @@
 $(function() {
+  /*
+  // Manual
   $("img").mouseenter(function(e) {
     $(this).next().css("display", "block")
   })
   $("img").mouseleave(function(e) {
     $(this).next().css("display", "none")
   })
+*/
+// Con toggle
+$("img").mouseenter(function(e) {
+  $(this).next().toggle("fast")
+})
+$("img").mouseleave(function(e) {
+  $(this).next().toggle("fast")
+})
 })
