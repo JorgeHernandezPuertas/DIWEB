1dd93f4ad7796e358302925582bb21acec914e6d
jorge <jherpue553@g.educaand.es>
2024-02-13 13:32:52 +0100

    Hago la tarea8

---
+++ jquery/practica8/scripts/eventos.js
@@ -0,0 +1,10 @@
+$(function () {
+  $("#mi-capa").click(function () {
+    $(this).css({
+      'background-color': '#0431B4',
+      'color': '#FFFF00',
+      'width': '35%',
+      'padding': '1.2rem'
+    })
+  })
+})
