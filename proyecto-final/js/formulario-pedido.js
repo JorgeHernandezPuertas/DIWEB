125091329092f74badc602430d77cf4e95be63af
JorgeHernandezPuertas <jherpue553@g.educaand.es>
2024-02-28 21:02:58 +0100

    afino el formulario de pedido con los errores

--- proyecto-final/js/formulario-pedido.js
+++ proyecto-final/js/formulario-pedido.js
@@ -1,13 +1,19 @@
 $(function () {
   $("section#pedido>form").on({
     "submit": function (e) {
+      e.preventDefault()
+      $("div.form-group>label>input")
       // Si ha rellenado todos los campos al pedir, le mando a la página de confirmación
       let noErrores = true
       const form = e.target
       for (let i = 0; i < form.length - 1; i++) {
         if (form[i].value === "") {
           noErrores = false
-          break
+          $(form[i]).addClass("error")
+        } else {
+          if ($(form[i]).hasClass("error")) {
+            $(form[i]).removeClass("error")
+          }
         }
       }
       if (noErrores) {
